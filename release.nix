65fa2c14603acf85bb0f3286fd1ed8c9342126af
carp-sushi <105142707+carp-sushi@users.noreply.github.com>
2022-10-09 12:15:39 -0600

    fetch tar ball

--- release.nix
+++ release.nix
@@ -1,6 +1,9 @@
 let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
+  nixpkgs = builtins.fetchTarball {
+    url    = "https://github.com/NixOS/nixpkgs/archive/ed9b904c5eba055a6d6f5c1ccb89ba8f0a056dc6.tar.gz";
+    sha256 = "0mq0i3saa5dihgppv54izvd5zw6isc8jdp328l2p7fnmq4nb8hws";
+  };
+  pkgs = import nixpkgs {};
 in
   { simple = pkgs.haskellPackages.callPackage ./simple.nix { };
   }
